24613d97fbdaa5d505d4c19f124b38f131d37474
opsaaaaa <sean@ferney.org>
2020-02-11 12:39:08 -0700

    Chip::Processor can process a document

---
+++ spec/chips_spec.rb
@@ -0,0 +1,51 @@
+RSpec.describe Chips do
+  it "has a version number" do
+    expect(Chips::VERSION).not_to be nil
+  end
+
+  context "Chips::Processor" do
+    let(:template_html) {'
+      <div>
+        <h1>
+          <div id="title"></div>
+        </h1>
+
+        <p>
+          <span id="description"></span>   
+        </p>
+      </div>    
+    '}
+
+    let(:output_html) {'
+      <div>
+        <h1>
+          Hello World!
+        </h1>
+
+        <p>
+          lorem ipsum
+        </p>
+      </div>
+    '}
+
+    it "can process a document" do
+      chips_output = Chips::Processor.new(
+          template_html,
+          mortises: {
+              'title': '#title',
+              'desc': '#description'
+          },
+          tenons: {
+              'title': 'Hello World!',
+              'desc': 'lorem ipsum'
+          }
+      ).generate()
+      
+      expect( chips_output.gsub(white_space ,'') ).to eq( output_html.gsub(white_space,'') )
+    end
+  end
+end
+
+def white_space
+  return /(^\s+|\s+$)/
+end
