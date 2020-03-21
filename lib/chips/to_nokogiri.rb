abdff1038c96f916f7f75ad8c3ee2ef0ae5cd32e
opsaaaaa <sean@ferney.org>
2020-03-21 12:08:50 -0600

    cleanup

--- lib/chips/to_nokogiri.rb
+++ lib/chips/to_nokogiri.rb
@@ -3,20 +3,19 @@ class NilClass
     return self
   end
 end
+
 class String
   def to_nokogiri
     return Nokogiri::HTML.fragment(self)
   end
 end
+
 class Hash
   def to_nokogiri
-    result = {}
-    self.each do |key,val|
-      result[key] = val.to_nokogiri
-    end
-    return result
+    return self.map { |k, str| [k, str.to_nokogiri] }.to_h
   end
 end
+
 class Array
   def to_nokogiri
     return self.map {|val| val.to_nokogiri}
